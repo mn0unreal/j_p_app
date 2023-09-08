6ddf1db33e66ea229320953d5f175c603b42528a
mn0unreal <mnfs2050@gmail.com>
2023-09-08 15:14:28 +0300

    Integrate admin theme
    - integrate admin template

---
+++ resources/views/layouts/admin/main.blade.php
@@ -0,0 +1,17 @@
+@include('layouts.admin.header')
+<body class="sb-nav-fixed">
+
+@include('layouts.admin.navbar')
+
+<div id="layoutSidenav">
+
+@include('layouts.admin.saidbar')
+
+<div id="layoutSidenav_content">
+@include('layouts.admin.body')
+@include('layouts.admin.footer')
+</div>
+</div>
+
+</body>
+</html>
